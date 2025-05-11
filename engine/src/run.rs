df1e1106de0e597eb575f6bccb737404aad94ecf
bulutcan99 <gocerbulutcan@gmail.com>
2025-05-11 11:01:09 +0300

    feat: add run events and outcome types

--- engine/src/run.rs
+++ engine/src/run.rs
@@ -1,1 +1,19 @@
+use bevy::prelude::*;
 
+#[derive(Event)]
+pub struct CyclePhaseEvent;
+
+#[derive(Event)]
+pub struct RunEndEvent {
+    pub outcome: RunOutcomeType,
+}
+
+pub enum RunOutcomeType {
+    Victory,
+    Defeat(RunDefeatType),
+}
+
+pub enum RunDefeatType {
+    PlayersDied,
+    TimeExpired,
+}
