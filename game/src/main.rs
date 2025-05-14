4e8dcb3d6222e0e75a96b9d997df1a63877625c7
bulutcan99 <gocerbulutcan@gmail.com>
2025-05-14 23:19:25 +0300

    feat: assets

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,11 +1,14 @@
+use arise_engine::states::AppStates;
 use bevy::prelude::*;
 
 mod camera;
 mod consts;
 mod development;
+mod player;
 
 fn main() {
     let mut app = App::new();
+    app.init_state::<AppStates>();
     app.insert_resource(ClearColor(Color::srgb_u8(1, 50, 45)));
     let bevy_plugins = DefaultPlugins;
     let bevy_plugins = bevy_plugins.set(ImagePlugin::default_nearest());
