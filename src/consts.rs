184b1316209301bc301be7d39a9f81c11004ab48
s84394880 <sevket.bulut.gocer@h-partners.com>
2025-05-29 12:09:36 +0300

    refactor

--- src/consts.rs
+++ src/consts.rs
@@ -1,2 +1,4 @@
 pub const WINDOW_WIDTH: f32 = 960.;
 pub const WINDOW_HEIGHT: f32 = 600.;
+
+pub const SIMULATION_SCALE_FACTOR: f32 = 1.0;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use bevy::app::PluginGroupBuilder;
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
+use bevy_rapier2d::prelude::*;
+use engine::states::AppStates;
 use options::display::DisplayConfig;
+use crate::consts::SIMULATION_SCALE_FACTOR;
+use crate::game::counters;
+use crate::options::PHYSICS_PIXELS_PER_METER;
 
 mod animation;
 mod camera;
@@ -10,7 +15,6 @@ mod dev;
 mod game;
 mod options;
 mod player;
-mod plugins;
 mod states;
 mod ui;
 mod combat;
@@ -18,7 +22,15 @@ mod run;
 mod scanner;
 mod spawnable;
 
-fn main() {}
+fn main() {
+    let display_config = set_display_config();
+    let mut app = build_app(
+        default_plugins(display_config),
+        custom_plugins(),
+    );
+
+    app.run();
+}
 
 // TODO: Player will set display!
 fn set_display_config() -> DisplayConfig {
@@ -32,4 +44,65 @@ fn default_plugins(display: DisplayConfig) -> PluginGroupBuilder {
             ..Default::default()
         })
         .set(ImagePlugin::default_nearest())
+}
+
+fn custom_plugins() -> PluginGroupBuilder {
+    #[allow(unused_mut)]
+    let mut res = ArisePlugins.build();
+    res
+}
+
+pub struct ArisePlugins;
+
+impl PluginGroup for ArisePlugins {
+    fn build(self) -> PluginGroupBuilder {
+        #[allow(unused_mut)]
+        // Allow because we might add more platform-specific features
+        let mut res = PluginGroupBuilder::start::<Self>()
+            .add(
+                RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(
+                    PHYSICS_PIXELS_PER_METER,
+                )
+                    .in_fixed_schedule(),
+            )
+            .add(dev::DevelopmentPlugin)
+            .add(states::StatesPlugin)
+            .add(animation::SpriteAnimationPlugin)
+            .add(game::GameResourcePlugin)
+            .add(counters::CounterPlugin)
+            .add(camera::CameraPlugin)
+            .add(combat::CombatPlugin)
+            .add(player::PlayerPlugin);
+
+        res
+    }
+}
+
+/// Make the runnable platform-specific app. `base_plugins` describes "external dependencies"
+/// outside the scope of the game itself. These typically come from `bevy::MinimalPlugins` or
+/// `bevy::DefaultPlugins`. `game_plugins` comes from from `ThetawaveGamePlugins`.
+fn build_app<P1: PluginGroup, P2: PluginGroup>(base_plugins: P1, game_plugins: P2) -> App {
+    // Should everything beside
+    let mut app = App::new();
+    app.add_plugins(base_plugins);
+    app.init_state::<AppStates>(); // start game in the main menu state
+    app.add_plugins(game_plugins);
+    app.insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(AmbientLight::default());
+
+    app.add_systems(
+        OnEnter(AppStates::Game),
+        setup_physics,
+    );
+    if cfg!(debug_assertions) && !cfg!(test) {
+        app.add_plugins(RapierDebugRenderPlugin::default());
+    }
+    app
+}
+
+fn setup_physics(mut commands: Commands, mut rapier_timestamp: ResMut<TimestepMode>) {
+    let rapier_config = RapierConfiguration::new(SIMULATION_SCALE_FACTOR);
+    commands.spawn(rapier_config);
+
+    *rapier_timestamp = TimestepMode::default();
 }
